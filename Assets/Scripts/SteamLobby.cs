a83c020e6619928eb2b1b9ad1e13e50698bcca60
Giyoung Kim <giy.kim27@gmail.com>
2026-02-24 18:54:50 -0800

    Allow players to host lobbies through Steam

---
+++ Assets/Scripts/SteamLobby.cs
@@ -0,0 +1,71 @@
+using Mirror;
+using UnityEngine;
+using Steamworks;
+
+public class SteamLobby : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject hostButton;
+
+    private NetworkManager networkManager;
+
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> lobbyEntered;
+
+    private const string HOST_ADDRESS_KEY = "HostAddress";
+
+    private void Start()
+    {
+        networkManager = GetComponent<NetworkManager>();
+
+        if (!SteamManager.Initialized)
+        {
+            return;
+        }
+
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+    }
+
+    public void HostLobby()
+    {
+        hostButton.SetActive(false);
+
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, networkManager.maxConnections);
+    }
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if (callback.m_eResult != EResult.k_EResultOK)
+        {
+            hostButton.SetActive(true);
+            return;
+        }
+
+        networkManager.StartHost();
+
+        SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HOST_ADDRESS_KEY, SteamUser.GetSteamID().ToString());
+    }
+
+    private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        if (NetworkServer.active)
+        {
+            return;
+        }
+
+        string hostAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HOST_ADDRESS_KEY);
+
+        networkManager.networkAddress = hostAddress;
+        networkManager.StartClient();
+
+        hostButton.SetActive(false);
+    }
+}
