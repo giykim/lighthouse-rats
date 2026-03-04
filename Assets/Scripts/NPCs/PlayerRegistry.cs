484e281507fb4d26eff03fb31f877d2d837c1c4f
Giyoung Kim <giy.kim27@gmail.com>
2026-03-03 17:25:26 -0800

    Reorganize files

--- Assets/Scripts/NPCs/PlayerRegistry.cs
+++ Assets/Scripts/NPCs/PlayerRegistry.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Add this component to your Player prefab.
-// EnemyAI reads from PlayerRegistry.All instead of FindGameObjectsWithTag every frame.
 public class PlayerRegistry : MonoBehaviour
 {
     public static readonly List<Transform> All = new();
